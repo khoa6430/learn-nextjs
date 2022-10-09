43f86483d3cd84ef3967a843147026d10864057b
DESKTOP-MCNF9AT\Khoa <khoa6430@gmail.com>
2022-10-09 08:37:07 +0700

    update login

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,7 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import httpProxy, { ProxyResCallback } from 'http-proxy'
+import httpProxy, { ProxyReqWsCallback, ProxyResCallback } from 'http-proxy'
 import { resolve } from 'path';
+import Cookies from 'cookies';
 type Data = {
   message: string
 }
@@ -14,7 +15,7 @@ export const config = {
 
 const proxy = httpProxy.createProxyServer();
 
-export default function handler( req: NextApiRequest, res: NextApiResponse<any>) {
+export default function handler( req: NextApiRequest, res: NextApiResponse<Data>) {
     if(req.method !== 'POST'){
         return res.status(404).json({message:'method not supported'})
     }
@@ -23,17 +24,31 @@ export default function handler( req: NextApiRequest, res: NextApiResponse<any>)
  
     console.log('login request');
     req.headers.cookie = ''
-    //api student
-    //http://js-post-api.herokuapp.com/api/students/
-    const handleLoginResponse:ProxyResCallback =  (proxyRes,req,res) => {
-        // target : process.env.API_URL,
-        target : 'https://js-post-api.herokuapp.com',
-        changeOrigin : true,
-        selfHandleResponse:true,
-    })
 
-    proxy.once('proxyRes',()=>{
-      resolve(true)
+    const handleLoginResponse:ProxyResCallback =  (proxyRes,req,res) => {
+        let body ='';
+        proxyRes.on('data',function(chunk){
+          body+=chunk
+        })
+        proxyRes.on('end',function(){
+          try{
+            const {accessToken,expiredAt} = JSON.parse(body)
+
+            //convert token to cookies
+            const cookies = new Cookies(req,res)
+
+            (res as NextApiResponse).status(200).json({message:'login successfully'})
+          }catch(error){
+            (res as NextApiResponse).status(500).json({message:'login sucessfully'})
+          }
+
+        })
+    };
+    proxy.once('proxyRes',handleLoginResponse);
+    proxy.web(req,res,{
+      target : process.env.API_URL,
+      changeOrigin : true,
+      selfHandleResponse:true,
     })
   })
 }
